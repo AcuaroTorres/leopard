ea3e437888ceb03e927595cc8de5615c5a29f85c
Alvaro Torres F <atorres@sauron.c.floxtor-sa.internal>
2017-12-23 13:10:48 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+
+## About UNISAG
+
+UNISAG es un esqueleto simple para aplicaciones.
+
+- Contiene Login de usuarios basados en RUN
+- Administración de usuarios
+- Roles de usuario
+
+## License
+
+Open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('password')->as('password.')->group(function(){
+	Route::get('edit', 'rrhh\UsersController@editPassword')->name('edit');
+	Route::put('update', 'rrhh\UsersController@updatePassword')->name('update');
+});
+
+Route::prefix('rrhh')->as('rrhh.')->group(function(){
+	Route::put('users/{user}/password', 'rrhh\UsersController@resetPassword')->name('users.password.reset');
+
+	Route::get('users/{user}/roles', 'rrhh\RolesController@index')->name('roles.index');
+	Route::post('users/{user}/roles','rrhh\RolesController@attach')->name('roles.attach');
+	
+	Route::resource('users','rrhh\UsersController')->middleware('auth');
+});
